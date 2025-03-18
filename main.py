1566cd316a7293be93d6cca70c1164bfd0caca6e
berkebaranbalci <berkebaranbalci@gmail.com>
2025-03-18 23:16:08 +0300

    Screen and Turtle added.

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+#Screen
+screen = turtle.Screen()
+screen.bgcolor("light blue")
+screen.title("Catch The Turtle Python")
+screen.setup(width=800 , height=600)
+
+#Create Turtle
+turtle_obj = turtle.Turtle()
+turtle_obj.shape("turtle")
+turtle_obj.color("green")
+turtle_obj.shapesize(2.2)
+
+
+
+
+
+
+
+
+
+
+screen.mainloop()
