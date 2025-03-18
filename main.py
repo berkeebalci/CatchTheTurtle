70e64651a01ca2605785631250f4e7de77860e9e
berkebaranbalci <berkebaranbalci@gmail.com>
2025-03-18 23:36:27 +0300

    Turtle random movement features added.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
 import turtle
+
 #Screen
 screen = turtle.Screen()
 screen.bgcolor("light blue")
@@ -10,6 +12,23 @@ turtle_obj = turtle.Turtle()
 turtle_obj.shape("turtle")
 turtle_obj.color("green")
 turtle_obj.shapesize(2.2)
+turtle_obj.penup()
+turtle_obj.speed(0)
+
+screen.tracer(0)
+
+def turtle_random_move():
+    x = random.randint(-280,280)
+    y = random.randint(-280,280)
+    turtle_obj.goto(x,y)
+    screen.update()
+    screen.ontimer(turtle_random_move,1000)
+turtle_random_move()
+
+
+
+
+
 
 
 
