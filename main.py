714df01dc2db06be727271693a216c7c157a19fd
berkebaranbalci <berkebaranbalci@gmail.com>
2025-03-19 00:47:46 +0300

    Count down and timer added.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+from sys import thread_info
 
 # Screen setting
 screen = turtle.Screen()
@@ -15,8 +16,17 @@ turtle_obj.shapesize(2.2)
 turtle_obj.penup()
 turtle_obj.speed(0)
 
-# Score
+# Score , timer and  record
+timer = 5
 score = 0
+record = 0
+
+#Timer Writer
+timer_writer = turtle.Turtle()
+timer_writer.hideturtle()
+timer_writer.penup()
+timer_writer.goto(300,260)
+timer_writer.color("Red")
 
 # Score Writer
 score_table = turtle.Turtle()
@@ -25,13 +35,34 @@ score_table.penup()
 score_table.goto(0, 260)
 score_table.color("darkblue")
 
+def time():
+    timer_writer.clear()
+    timer_writer.write(f"Time: {timer}",align="center",font=("Arial", 16, "bold"))
+
+def game_over():
+    turtle_obj.hideturtle()
+    score_table.goto(0 , 0)
+    score_table.write("GAME OVER!", align="center",font=("Arial",24,"bold"))
+
+def count_down():
+    global timer
+    if timer > 0:
+        timer -= 1
+        time()
+        screen.ontimer(count_down,1000)
+    else:
+        game_over()
+
+
 def score_writer():
     score_table.clear()
-    score_table.write(f"Skor: {score}", align="center", font=("Arial", 20, "bold"))
+    score_table.write(f"Skor: {score} | Record:{record}", align="center", font=("Arial", 20, "bold"))
 
 def catch(x, y):
-    global score
+    global score , record
     score += 1
+    if score > record:
+        record = score
     score_writer()
 
 def turtle_move():
@@ -40,8 +71,14 @@ def turtle_move():
     turtle_obj.goto(x, y)
     screen.ontimer(turtle_move, 1000)
 
+
+
+
+
 # Starters
 turtle_obj.onclick(catch)
 score_writer()
+time()
+count_down()
 turtle_move()
 screen.mainloop()
